9ceb74844e870ddadd10020e6176e7e4bfd24a64
Vitor Aliaga <aliagavitor@gmail.com>
2025-05-01 21:15:15 -0300

    Changing page message

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
 
-    return <h1>Bruna, eu amo você. Se você também me ama, dá uma risadinha!</h1>;
+    return <h1>Bruna, eu amo você. Se você também me ama, dê uma risadinha!</h1>;
 
 }
 
