38a4b8f2a0287002aff3b65af9ff66ba468a0e02
Vitor Aliaga <vitoraliaga@Vitors-MacBook-Pro.local>
2025-05-12 20:35:52 -0300

    Adding scripts `lint:check e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para entender como montar um aplicativo do 0 a 100%
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
 import { useState } from "react";
 
 function Home() {
-    const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-    const handleClick = () => {
-        setClicked(true);
-    };
+  const handleClick = () => {
+    setClicked(true);
+  };
 
-    return (
-        <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "50px" }}>
-            <h1>Bruna, eu amo você. Se você também me ama, dê uma risadinha! 😎</h1>
-            {!clicked ? (
-                <button onClick={handleClick} style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}>
-                    😄 Hehehe
-                </button>
-            ) : (
-                <h2>Sabia que você me ama! Ansioso pela nossa viagem logo. 💘</h2>
-            )}
-        </div>
-    );
+  return (
+    <div
+      style={{
+        fontFamily: "sans-serif",
+        textAlign: "center",
+        marginTop: "50px",
+      }}
+    >
+      <h1>Bruna, eu amo você. Se você também me ama, dê uma risadinha! 😎</h1>
+      {!clicked ? (
+        <button
+          onClick={handleClick}
+          style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
+        >
+          😄 Hehehe
+        </button>
+      ) : (
+        <h2>Sabia que você me ama! Ansioso pela nossa viagem logo. 💘</h2>
+      )}
+    </div>
+  );
 }
 
 export default Home;
